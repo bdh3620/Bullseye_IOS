26d8258d9a1c6af9a524f106b94e81dbf6def0be
bdh3620 <bdh3620@naver.com>
2022-03-07 15:52:56 +0900

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by 백대홍 on 2022/03/07.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
